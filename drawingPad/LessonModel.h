6f99f44963393bff00ca588db6e6a08f01ecd5bb
iOS Apps Development <info@iosappsdev.org>
2014-06-20 22:57:14 -0700

    Created Model

---
+++ drawingPad/LessonModel.h
@@ -0,0 +1,17 @@
+//
+//  LessonModel.h
+//  drawingPad
+//
+//  Created by Ricardo Ruiz on 6/20/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LessonModel : NSObject
+
+@property (nonatomic,strong) NSString *lessonID;
+@property (nonatomic,strong) NSString *title;
+@property (nonatomic,strong) NSArray *symbols;
+
+@end
---
+++ drawingPad/LessonModel.m
@@ -0,0 +1,30 @@
+//
+//  LessonModel.m
+//  drawingPad
+//
+//  Created by Ricardo Ruiz on 6/20/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import "LessonModel.h"
+
+@implementation LessonModel {
+ 
+    NSArray *lesson1;
+}
+@synthesize title, symbols, lessonID;
+
+-(id)init {
+    if (self == nil) {
+       return [super init];
+    }
+    
+    
+    
+    
+    
+    
+    
+    return self;
+}
+@end
