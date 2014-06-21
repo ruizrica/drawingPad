674ee8a9b8273c6eafdea7f6021952a509a8b9c7
iOS Apps Development <info@iosappsdev.org>
2014-06-21 14:59:14 -0700

    Added Symbols for Lesson 1
    Additional Code Changes

--- drawingPad/LessonModel.m
+++ drawingPad/LessonModel.m
@@ -8,16 +8,32 @@
 
 #import "LessonModel.h"
 
-@implementation LessonModel {
- 
-    NSArray *lesson1;
-}
+@implementation LessonModel
 @synthesize title, symbols, lessonID, userInput;
 
+// Lessons
+@synthesize lesson_1;
+
 - (id)init {
-    if (self == nil) {
-       return [super init];
+    self = [super init];
+    if (self) {
+        
+        lesson_1 = @[@"symbol_1.png",@"symbol_2.png",@"symbol_3.png",@"symbol_4.png",@"symbol_5.png",@"symbol_6.png",@"symbol_7.png",@"symbol_8.png",@"symbol_9.png",@"symbol_10.png"];
     }
     return self;
+}
+
+
+- (NSArray *)getLessonWithID:(int)lesson_ID {
+    
+    NSArray *selectedArray = [[NSArray alloc]init];
+    switch (lesson_ID) {
+        case 1: selectedArray = lesson_1;
+            break;
+    // Need Rest of Lessons
+        default:
+            break;
+    }
+    return selectedArray;
 }
 @end
