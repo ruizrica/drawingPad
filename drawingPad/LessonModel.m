ffc797eedad667938ce77c8ace1b7edae7f0bcf5
iOS Apps Development <info@iosappsdev.org>
2014-06-27 09:41:12 -0700

    Additional Code Updates
    Add

--- drawingPad/LessonModel.m
+++ drawingPad/LessonModel.m
@@ -12,18 +12,93 @@
 @synthesize symbols, userInput;
 
 // Lesson 1
-@synthesize lesson_1A;
-
+@synthesize lesson_1,lesson_1A,lesson_1B,lesson_2C,lesson_1D,lesson_1E;
 // Lesson 2
-@synthesize lesson_2A,lesson_2B,lesson_2C,lesson_2D,lesson_2E;
+@synthesize lesson_2,lesson_2A,lesson_2B,lesson_2C,lesson_2D,lesson_2E;
+// Lesson 3
+@synthesize lesson_3,lesson_3A,lesson_3B,lesson_3C,lesson_3D,lesson_3E;
+// Lesson 4
+@synthesize lesson_4,lesson_4A,lesson_4B,lesson_4C,lesson_4D,lesson_4E;
+// Lesson 5
+@synthesize lesson_5,lesson_5A,lesson_5B,lesson_5C,lesson_5D,lesson_5E;
+// Lesson 6
+@synthesize lesson_6,lesson_6A,lesson_6B,lesson_6C,lesson_6D,lesson_6E;
+// Lesson 7
+@synthesize lesson_7,lesson_7A,lesson_7B,lesson_7C,lesson_7D,lesson_7E;
+// Lesson 8
+@synthesize lesson_8,lesson_8A,lesson_8B,lesson_8C,lesson_8D,lesson_8E;
+// Lesson 9
+@synthesize lesson_9,lesson_9A,lesson_9B,lesson_9C,lesson_9D,lesson_9E;
+// Lesson 10
+@synthesize lesson_10,lesson_10A,lesson_10B,lesson_10C,lesson_10D,lesson_10E;
+// Lesson 11
+@synthesize lesson_11,lesson_11A,lesson_11B,lesson_11C,lesson_11D,lesson_11E;
+// Lesson 12
+@synthesize lesson_12,lesson_12A,lesson_12B,lesson_12C,lesson_12D,lesson_12E;
+// Lesson 13
+@synthesize lesson_13,lesson_13A,lesson_13B,lesson_13C,lesson_13D,lesson_13E;
+// Lesson 14
+@synthesize lesson_14,lesson_14A,lesson_14B,lesson_14,lesson_14D,lesson_14E;
+// Lesson 15
+@synthesize lesson_15,lesson_15A,lesson_15B,lesson_15C,lesson_15D,lesson_15E;
+// Lesson 16
+@synthesize lesson_16,lesson_16A,lesson_16B,lesson_16C,lesson_16D,lesson_16E;
+// Lesson 17
+@synthesize lesson_17,lesson_17A,lesson_17B,lesson_17C,lesson_17D,lesson_17E;
+// Lesson 18
+@synthesize lesson_18,lesson_18A,lesson_18B,lesson_18C,lesson_18D,lesson_18E;
+// Lesson 19
+@synthesize lesson_19,lesson_19A,lesson_19B,lesson_19C,lesson_19D,lesson_19E;
+// Lesson 20
+@synthesize lesson_20,lesson_20A,lesson_20B,lesson_20C,lesson_20D,lesson_20E;
 
+
+#pragma mark - Methods
 - (NSArray *)getLessonWithID:(int)lesson_ID {
     
     NSArray *selectedArray = [[NSArray alloc]init];
     switch (lesson_ID) {
-        case 1: selectedArray = lesson_1A;
+        case 1: selectedArray = lesson_1;
             break;
-            // Need Rest of Lessons
+        case 2: selectedArray = lesson_2;
+            break;
+        case 3: selectedArray = lesson_3;
+            break;
+        case 4: selectedArray = lesson_4;
+            break;
+        case 5: selectedArray = lesson_5;
+            break;
+        case 6: selectedArray = lesson_6;
+            break;
+        case 7: selectedArray = lesson_7;
+            break;
+        case 8: selectedArray = lesson_8;
+            break;
+        case 9: selectedArray = lesson_9;
+            break;
+        case 10: selectedArray = lesson_10;
+            break;
+        case 11: selectedArray = lesson_11;
+            break;
+        case 12: selectedArray = lesson_12;
+            break;
+        case 13: selectedArray = lesson_13;
+            break;
+        case 14: selectedArray = lesson_14;
+            break;
+        case 15: selectedArray = lesson_15;
+            break;
+        case 16: selectedArray = lesson_16;
+            break;
+        case 17: selectedArray = lesson_17;
+            break;
+        case 18: selectedArray = lesson_18;
+            break;
+        case 19: selectedArray = lesson_19;
+            break;
+        case 20: selectedArray = lesson_20;
+            break;
+            
         default:
             break;
     }
@@ -34,11 +109,24 @@
     self = [super init];
     if (self) {
         
-        // Image Arrays
-        lesson_1A = @[@"symbol_1.png",@"symbol_2.png",@"symbol_3.png",@"symbol_4.png",@"symbol_5.png",@"symbol_6.png",@"symbol_7.png",@"symbol_8.png",@"symbol_9.png",@"symbol_10.png"];
-
-        // Text Arrays
+        // Image Arrays:
+        lesson_1 = @[@"symbol_1.png",@"symbol_2.png",@"symbol_3.png",@"symbol_4.png",@"symbol_5.png",@"symbol_6.png",@"symbol_7.png",@"symbol_8.png",@"symbol_9.png",@"symbol_10.png"];
+        lesson_2 = @[];
+        lesson_3 = @[];
+        lesson_4 = @[];
+        lesson_5 = @[];
+        lesson_6 = @[];
+        lesson_7 = @[];
+        lesson_8 = @[];
+        lesson_9 = @[];
         
+        // Lesson 1
+        lesson_1A = @[];
+        lesson_1B = @[];
+        lesson_1C = @[];
+        lesson_1D = @[];
+        lesson_1E = @[];
+
         // Lesson 2
         lesson_2A =@[@""];
         lesson_2B = @[@"9",@"3", @"8", @"2", @"4",@"13", @"82", @"31", @"99", @"67"];
@@ -153,8 +241,6 @@
         lesson_20B = @[@"any",@"best",@"away",@"these", @"right", @"could", @"cold",@"three", @"who", @"down"];
         lesson_20C = @[@"by",@"fast",@"show",@"these", @"ride", @"eyes", @"away",@"right", @"face", @"people"];
         lesson_20D = @[@"ride",@"eyes",@"show",@"face", @"right", @"by", @"people",@"these", @"fast", @"away"];
-         
-         
     }
     return self;
 }
