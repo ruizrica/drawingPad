2dccc8c2bc5e78d2d3dfc16a61885b3e5d38dd6c
iOS Apps Development <info@iosappsdev.org>
2014-07-01 18:52:44 -0700

    Created New Class

---
+++ drawingPad/StoryCardView.h
@@ -0,0 +1,13 @@
+//
+//  StoryCardView.h
+//  drawingPad
+//
+//  Created by Ricardo Ruiz on 7/1/14.
+//  Copyright (c) 2014 iOS Apps Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryCardView : UIViewController
+
+@end
